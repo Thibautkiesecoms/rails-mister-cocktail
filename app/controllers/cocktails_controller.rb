53e18d07e439cddfab76d0eb255f9940e6020df6
francois <kiesecoms.thibaut@gmail.com>
2019-08-09 15:22:21 +0200

    start html

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,43 +1,48 @@
 class CocktailsController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
-  end
 
-  def create
-    @cocktail = Cocktail.create(cocktail_params)
-    if @cocktail.save
-     redirect_to cocktail_path(@cocktail)
-    else
-      render :new
-    end
   end
 
   def new
     @cocktail = Cocktail.new
   end
 
-  def edit
-  end
-
-  def update
-     @cocktail = Cocktail.update(cocktail_params)
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-     redirect_to cocktail_path(@cocktail)
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
   end
 
-  def delete
+
+  def edit
   end
-end
+
+  def update
+    @cocktail.update(cocktail_params)
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
 
 private
+
 
   def cocktail_params
     params.require(:cocktail).permit(:name)
   end
+  def set_task
+    @cocktail = Cocktail.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :cocktails do
-    resources :doses, only: :create
+    resources :doses, only: [:create, :new]
   end
-  resources :ingredient
+  resources :doses, only: [:destroy]
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
